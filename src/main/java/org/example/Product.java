e3316077da75ca792144d76b8a1babe54a421d77
med <mohamedelafia732@gmail.com>
2024-12-20 16:14:30 +0100

    first

---
+++ src/main/java/org/example/Product.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Product {
+    private String name ;
+    private float price ;
+    public Product(String name , float price ) {
+        this.price = price ;
+        this.name = name ;
+    }
+}
